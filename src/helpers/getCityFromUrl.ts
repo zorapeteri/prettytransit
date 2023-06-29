6e5388a45cf0ab8ea810e38efc7495cf4ede7cff
Zora Péteri <zorapeteri@gmail.com>
2023-06-29 19:18:24 +0200

    fix cities lookup type

--- src/helpers/getCityFromUrl.ts
+++ src/helpers/getCityFromUrl.ts
@@ -2,7 +2,7 @@ import cities from '@/constants/cities'
 
 export function getCityFromUrl(): string {
   const city = (location.pathname.split('/').filter(Boolean)[0] || '').toLowerCase()
-  if (!cities.includes(city)) {
+  if (!(cities as any).includes(city)) {
     const startsWith = cities.find((_city) => _city.startsWith(city))
     if (startsWith) return startsWith
     location.assign('/')
