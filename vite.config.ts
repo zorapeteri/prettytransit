07441557958734757aa0ca565937df2498168f19
Zora Péteri <zorapeteri@gmail.com>
2023-07-14 19:12:28 +0200

    add bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer() as PluginOption],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
