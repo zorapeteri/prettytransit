880398f9a404f34926932d4a34ba8e8a4e2ae679
Zora Péteri <zorapeteri@gmail.com>
2023-07-15 14:09:10 +0200

    fix selector for meta description

--- src/helpers/setMetaForCity.ts
+++ src/helpers/setMetaForCity.ts
@@ -13,6 +13,6 @@ export function setMetaForCity(city: string) {
   document.title = title
   document.querySelector('meta[property="og:title"]')?.setAttribute('content', title)
 
-  document.querySelector('meta[property="description"]')?.setAttribute('content', description)
+  document.querySelector('meta[name="description"]')?.setAttribute('content', description)
   document.querySelector('meta[property="og:description"]')?.setAttribute('content', description)
 }
