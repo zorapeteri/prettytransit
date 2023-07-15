6d5b345f62c8ad1f405018ee937c989f5addb6a9
Zora Péteri <zorapeteri@gmail.com>
2023-07-15 13:46:26 +0200

    redirect to home if multiple city matches

--- src/helpers/getCityFromUrl.ts
+++ src/helpers/getCityFromUrl.ts
@@ -4,8 +4,8 @@ import { cityNames } from '@/constants/cityNames'
 export function getCityFromUrl(): string {
   const city = (location.pathname.split('/').filter(Boolean)[0] || '').toLowerCase()
   if (!(cities as any).includes(city)) {
-    const startsWith = cities.find((_city) => _city.startsWith(city))
-    if (startsWith) return startsWith
+    const startsWith = cities.filter((_city) => _city.startsWith(city))
+    if (startsWith.length === 1) return startsWith[0]
     location.assign('/')
     return ''
   }
